54a6c00ad5ff800fb2d410dcbf76ce9c2297cc77
fcabrera-c <75145601+fcabrera-c@users.noreply.github.com>
2021-08-24 23:22:17 -0500

    Update login-steps.js

--- Login/src/test/resources/steps/login-steps.js
+++ Login/src/test/resources/steps/login-steps.js
@@ -3,12 +3,14 @@ Given(/^we access on the login page$/, function() {
   browser.configure().window().maximize();
   var timeUnits = require("minium/timeunits");
   $(":root").waitTime(3, timeUnits.SECONDS);
+  scenario.embed(browser.screenshot().asBytes(), "image/png");
 });
 
 Given(/^we capture all data of the login page fields$/, function() {
   $("#25").click();
   $("#25").sendKeys("4452900102030466");
   $("#39").sendKeys("44529001");
+  scenario.embed(browser.screenshot().asBytes(), "image/png");
   $(":root").waitTime(3, timeUnits.SECONDS);
   //$("#46").click();
   //$("#app > div.login.login--ctn.ibk-login-layout-layout.ibk-login-layout__background-blur > div.ibk-login-layout__background > div.ibk-login-layout__wrapper > div.ibk-login-layout__page-nav > div > div > div > div.ibk-auth-box-body > div > div:nth-child(2) > div > div.ibk-keyboard-virtual > div > div").moveTo();
@@ -26,6 +28,7 @@ Given(/^we capture the second option$/, function() {
   $("#documentType > ul > li:nth-child(2)").click();
   $(":root").waitTime(1, timeUnits.SECONDS);
   $("#39").sendKeys("44529002");
+  scenario.embed(browser.screenshot().asBytes(), "image/png");
   $(":root").waitTime(3, timeUnits.SECONDS);
 });
 
@@ -35,6 +38,7 @@ Given(/^we capture the thirth option$/, function() {
   $("#documentType > ul > li:nth-child(3)").click();
   $(":root").waitTime(1, timeUnits.SECONDS);
   $("#39").sendKeys("TE529003003");
+  scenario.embed(browser.screenshot().asBytes(), "image/png");
   $(":root").waitTime(3, timeUnits.SECONDS);
 });
 
@@ -55,6 +59,7 @@ When(/^we select the password field$/, function() {
   $(":root").waitTime(1, timeUnits.SECONDS);
   $("#app > div.login.login--ctn.ibk-login-layout-layout.ibk-login-layout__background-blur > div.ibk-login-layout__background > div.ibk-login-layout__wrapper > div.ibk-login-layout__page-nav > div > div > div > div.ibk-auth-box-body > div > div:nth-child(2) > div > div.ibk-keyboard-virtual > div > div > div > a:nth-child(8)").click();
   //$("#app > div.login.login--ctn.ibk-login-layout-layout.ibk-login-layout__background-blur > div.ibk-login-layout__background > div.ibk-login-layout__wrapper > div.ibk-login-layout__page-nav > div > div > div > div.ibk-auth-box-body > div > button").click();
+  scenario.embed(browser.screenshot().asBytes(), "image/png");
   $(":root").waitTime(2, timeUnits.SECONDS);
 });
 Then(/^we select the check box Recordar datos$/, function() {
