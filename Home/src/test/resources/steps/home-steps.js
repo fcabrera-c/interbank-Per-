72485c3ebb34a036c0b562be696c5a41d9370d97
fcabrera-c <75145601+fcabrera-c@users.noreply.github.com>
2021-08-24 22:53:05 -0500

    Update home-steps.js

--- Home/src/test/resources/steps/home-steps.js
+++ Home/src/test/resources/steps/home-steps.js
@@ -3,14 +3,18 @@ Given(/^we access on the home page$/, function() {
   browser.configure().window().maximize();
   var timeUnits = require("minium/timeunits");
   $(":root").waitTime(5, timeUnits.SECONDS);
+  scenario.embed(browser.screenshot().asBytes(), "image/png");
 });
 Given(/^we validate all sections of the page$/, function() {
   $(":root").waitTime(5, timeUnits.SECONDS);
   $("#p_p_id_56_INSTANCE_VAvq3YGSL62w_ > div > div > div.journal-content-article > div").scrollIntoView();
+  scenario.embed(browser.screenshot().asBytes(), "image/png");
   $(":root").waitTime(3, timeUnits.SECONDS);
   $("#cta-bar__content-js > div.lh-cta-bar__content").scrollIntoView();
+  scenario.embed(browser.screenshot().asBytes(), "image/png");
   $(":root").waitTime(3, timeUnits.SECONDS);
   $("#p_p_id_56_INSTANCE_apC6xMhAPCrE_ > div > div > div.journal-content-article").scrollIntoView();
+  scenario.embed(browser.screenshot().asBytes(), "image/png");
   $(":root").waitTime(3, timeUnits.SECONDS);
 });
 
